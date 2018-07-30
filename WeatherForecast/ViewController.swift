723e427909618c1b684c52ee8909c4a0ef810f3c
Melany Lourens <lourensmelany@gmail.com>
2018-07-30 21:02:58 +0200

    Add basic UI and assets

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Melany Lourens on 2018/07/28.
+//  Copyright © 2018 Melany Lourens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
